ffe9845d0ece3584969591f86d144dbe539d8524
0xlunar <20531468+0xlunar@users.noreply.github.com>
2024-02-12 22:59:44 +1000

    Added Clap

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,39 @@
 use std::path::Path;
-use std::time::Duration;
 use sysinfo;
 use sysinfo::System;
 use crate::core::injection::InjectionMethod::Standard;
+use clap::Parser;
 
 mod core;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Arguments {
+    #[arg(short, long)]
+    name: String,
+    #[arg(short, long)]
+    dll: String,
+}
+
 fn main() {
+    let args = Arguments::parse();
+
     let system = System::new_all();
-    let pid = match system.processes_by_name("injector_test").next() {
+    let pid = match system.processes_by_name(&args.name).next() {
         Some(t) => t,
         None => {
-            panic!("Process not found");
+            println!("Process not found");
+            std::process::exit(0);
         }
     };
 
-    let dll = Path::new("E:\\PROJECTS\\TOOLS\\example_dll\\target\\release\\example_dll.dll");
+    println!("Targeting: {}, PID: {}", pid.name(), pid.pid().as_u32());
+
+    let dll = Path::new(&args.dll);
     let target = core::injection::Target::new(pid, dll, Standard);
     match target.inject() {
         Ok(t) => println!("Outcome: {:?}", t.memory_address),
         Err(e) => println!("Error: {:?}", e),
     }
-
-    std::thread::sleep(Duration::from_secs(10));
 }
 
